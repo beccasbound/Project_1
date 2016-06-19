4404de4f0ae5e1e7d71afb62f839849c9d71b186
beccasbound <rebecca.s.peabody@gmail.com>
2016-06-19 08:35:06 -0500

    commented code in Main

--- VSProject_1/Project_1_Main.cpp
+++ VSProject_1/Project_1_Main.cpp
@@ -8,6 +8,13 @@ using namespace std;
 
 int main() {
 
+    /*
+    Assignment_Book my_assignments;
+    my_assignments.load("assignments.txt");
+    UserInterface menu(my_assignments);
+    menu.main();
+    */
+
 	UserInterface menu;
 	menu.main();
 
